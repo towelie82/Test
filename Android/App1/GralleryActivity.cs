dad4f958518fe8611739f9fddfce11525be17b8f
wendyzang@hotmail.com <t%R8x2Qv>
2019-10-14 11:24:44 +0800

    upload

---
+++ Android/App1/GralleryActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "GralleryActivity")]
+    public class GralleryActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.GralleryLayout);
+
+            var ImageView = FindViewById<ImageView>(Resource.Id.imageView1);
+            ImageView.SetImageResource(Resource.Drawable.pig);
+        }
+    }
+}
---
+++ Android/App1/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var btn_TextLayout = FindViewById<Button>(Resource.Id.btn_TextLayout);
+            btn_TextLayout.Click += Btn_TextLayout_Click;
+
+            var btn_GralleryLayout = FindViewById<Button>(Resource.Id.btn_GralleryLayout);
+            btn_GralleryLayout.Click += Btn_GralleryLayout_Click;
+        }
+
+        private void Btn_GralleryLayout_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(GralleryActivity));
+            StartActivity(intent);
+        }
+
+        private void Btn_TextLayout_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(TextActivity));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
