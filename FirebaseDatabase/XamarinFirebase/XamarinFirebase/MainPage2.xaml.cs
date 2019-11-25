9e0e1e8f9a2347ef9dd3295dc9a3302f884287fb
wendyzang@hotmail.com <t%R8x2Qv>
2019-11-25 13:36:06 +0800

    firebase databse

---
+++ FirebaseDatabase/XamarinFirebase/XamarinFirebase/MainPage2.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFirebase.Helper;
+
+namespace XamarinFirebase
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage2 : ContentPage
+    {
+        FirebaseHelper2 firebaseHelper2 = new FirebaseHelper2();
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+
+            base.OnAppearing();
+            var allPersons = await firebaseHelper2.GetAllCharacters();
+            lstPersons.ItemsSource = allPersons;
+        }
+
+        private async void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+            await firebaseHelper2.AddCharacter(txtName.Text, txtEmail.Text, txtLastName.Text);
+            txtEmail.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtName.Text = string.Empty;
+            await DisplayAlert("Success", "Person Added Successfully", "OK");
+            var allPersons = await firebaseHelper2.GetAllCharacters();
+            lstPersons.ItemsSource = allPersons;
+        }
+
+        private async void BtnRetrive_Clicked(object sender, EventArgs e)
+        {
+            var character = await firebaseHelper2.GetEmail(txtEmail.Text);
+            if (character != null)
+            {
+                txtEmail.Text = character.Email;
+                txtName.Text = character.Name;
+                txtLastName.Text = character.LastName;
+                await DisplayAlert("Success", "character Retrive Successfully", "OK");
+
+            }
+            else
+            {
+                await DisplayAlert("Success", "No character Available", "OK");
+            }
+
+        }
+
+        private async void BtnUpdate_Clicked(object sender, EventArgs e)
+        {
+            await firebaseHelper2.UpdatePerson(txtEmail.Text, txtName.Text, txtLastName.Text);
+            txtEmail.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtName.Text = string.Empty;
+            await DisplayAlert("Success", "Person Updated Successfully", "OK");
+            var allPersons = await firebaseHelper2.GetAllCharacters();
+            lstPersons.ItemsSource = allPersons;
+        }
+
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            await firebaseHelper2.DeletePerson(txtEmail.Text);
+            await DisplayAlert("Success", "Person Deleted Successfully", "OK");
+            var allPersons = await firebaseHelper2.GetAllCharacters();
+            lstPersons.ItemsSource = allPersons;
+        }
+
+        private async void btnQuery_Clicked(object sender, EventArgs e)
+        {
+            var character = await firebaseHelper2.GetEmail2(txtEmail.Text);
+            if (character != null)
+            {
+                txtEmail.Text = character.Email;
+                txtName.Text = character.Name;
+                txtLastName.Text = character.LastName;
+
+            }
+        }
+    }
+}
---
+++ FirebaseDatabase/XamarinFirebase/XamarinFirebase/Model/Character.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFirebase.Model
+{
+    public class Character
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
