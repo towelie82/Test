e1a395df0b7b728e860237c07dc9c73c1d2e5988
wendyzang@hotmail.com <t%R8x2Qv>
2019-11-25 16:46:59 +0800

    tabbed page test

---
+++ TabbedPageDemo/TabbedPageDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabbedPageDemo.Views;
+using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_imgAdd_Clicked(object sender, EventArgs e)
+        {
+            var tabbedPage = new TabbedPage();
+            tabbedPage.Children.Add(new Page1());
+            tabbedPage.Children.Add(new Page2());
+
+            App.Current.MainPage = tabbedPage;
+        }
+    }
+}
