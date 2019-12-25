21c39ad1469b0da215b59c91f34b029d08959c5e
wendyzang@hotmail.com <t%R8x2Qv>
2019-12-25 17:06:09 +0800

    hide keyboard of entry

---
+++ HidekeyboardOfEntryDemo/HidekeyboardOfEntryDemo/HidekeyboardOfEntryDemo.iOS/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using HidekeyboardOfEntryDemo;
+using HidekeyboardOfEntryDemo.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace HidekeyboardOfEntryDemo.iOS
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        private UITextField textField;
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            textField = (UITextField)this.Control;
+            textField.ReturnKeyType = UIReturnKeyType.Done;
+        }
+    }
+}
---
+++ HidekeyboardOfEntryDemo/HidekeyboardOfEntryDemo/HidekeyboardOfEntryDemo/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HidekeyboardOfEntryDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Person> _list { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            _list = new ObservableCollection<Person>()
+            {
+                new Person(){Id ="0"},
+                new Person(){Id ="1"}
+            };
+
+            this.BindingContext = this;
+        }
+
+        private void AddMore_Tapped(object sender, EventArgs e)
+        {
+            _list.Add(new Person() { Id = "1" });
+        }
+    }
+    public class Person : INotifyPropertyChanged
+    {
+        private string _Id;
+        public string Id
+        {
+            get
+            {
+                return _Id;
+            }
+            set
+            {
+                _Id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
