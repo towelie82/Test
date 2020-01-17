f9dcd6a8ba59dd35c2f164e9ae050f8574ba4778
wendyzang@hotmail.com <t%R8x2Qv>
2020-01-17 15:14:33 +0800

    upload camera_RuntimePermission_WebView_Xamarin.Forms

---
+++ CameraRuntimePermission_WebView/WebViewDemo/WebViewDemo/WebViewDemo/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WebViewDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void _button_Clicked(object sender, EventArgs e)
+        {
+            webView.Source = "https://test.webrtc.org/";//"https://xamarin.swappsdev.net/";
+        }
+    }
+}
