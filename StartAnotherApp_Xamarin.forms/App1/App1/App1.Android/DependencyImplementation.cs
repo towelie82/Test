36b94067a772a0fa8bea2985fe89a76d24dd891d
wendyzang@hotmail.com <t%R8x2Qv>
2020-02-10 13:08:38 +0800

    upload how to start another app in xamarin forms

---
+++ StartAnotherApp_Xamarin.forms/App1/App1/App1.Android/DependencyImplementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace App1.Droid
+{
+    public class DependencyImplementation : Activity, IDpendencyService
+    {
+        public Task<bool> Launch(string stringUri)
+        {
+
+            Intent intent = Android.App.Application.Context.PackageManager.GetLaunchIntentForPackage(stringUri);
+            if (intent != null)
+            {
+                intent.AddFlags(ActivityFlags.NewTask);
+                Forms.Context.StartActivity(intent);
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return Task.FromResult(true);
+            }
+
+
+
+        }
+    }
+}
---
+++ StartAnotherApp_Xamarin.forms/App1/App1/App1.Android/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Xamarin.Forms;
+using App1;
+
+[assembly: Dependency(typeof(IDpendencyService))]
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class Activity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public Intent intent;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            DependencyService.Register<IDpendencyService, DependencyImplementation>();
+            
+            //SetContentView(Resource.Layout.layout1);
+
+            //var textview1 = FindViewById<TextView>(Resource.Id.textView1);
+            //textview1.Text = "App1";
+
+            //var button1 = FindViewById<Button>(Resource.Id.button1);
+            //button1.Click += delegate
+            //{
+            //    Intent intent = PackageManager.GetLaunchIntentForPackage("com.companyname.app2");
+            //    StartActivity(intent);
+            //};
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
