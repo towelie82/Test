010088182dd46b2a61041da372da6014778fa31c
wendyzang@hotmail.com <t%R8x2Qv>
2020-05-15 10:54:03 +0800

    upload

---
+++ Converters/inverterDemo/inverterDemo/inverterDemo/BooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace inverterDemo
+{
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !System.Convert.ToBoolean(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !System.Convert.ToBoolean(value);
+        }
+    }
+}
---
+++ Converters/inverterDemo/inverterDemo/inverterDemo/FrameButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace inverterDemo
+{
+    public class FrameButton : Frame
+    {
+        public string IsBusy { get; set; }
+        public FrameButton()
+        {
+            var MyTapGesture = new TapGestureRecognizer();
+            MyTapGesture.Tapped += (sender, e) => { Clicked(); };
+            GestureRecognizers.Add(MyTapGesture);
+        }
+
+        public async void Clicked()
+        {
+            await this.ScaleTo(1.1, 100);
+            await this.ScaleTo(1, 100);
+            Console.WriteLine(this.IsEnabled);
+        }
+    }
+}
