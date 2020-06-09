96614e26560e4878f22a65f8a3bbbfc3b5452672
wendyzang@hotmail.com <t%R8x2Qv>
2020-06-09 13:24:25 +0800

    upload

---
+++ IndicatorView/IndicatorViewDemos/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IndicatorViewDemos.Models
+{
+    public class Items
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public ImageSource ImageSource { get; set; }
+    }
+}
---
+++ IndicatorView/IndicatorViewDemos/Views/BasicIndicatorViewPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using IndicatorViewDemos.Models;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IndicatorViewDemos.Views
+{
+    public partial class BasicIndicatorViewPage : ContentPage
+    {
+        public IList<Items> items { get; set; }
+        public BasicIndicatorViewPage()
+        {
+            InitializeComponent();
+            items = new List<Items>()
+            {
+                new Items(){ Title="A", Subtitle="S", ImageSource="pink.jpg"},
+                new Items(){ Title="B", Subtitle="S", ImageSource="pink.jpg"},
+                new Items(){ Title="C", Subtitle="S", ImageSource="pink.jpg"},
+            };
+            this.BindingContext = this;
+        }
+    }
+}
