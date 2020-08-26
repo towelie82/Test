b9aa4907360565c67cde909ae5b1dce6dedef895
wendyzang@hotmail.com <t%R8x2Qv>
2020-08-26 15:42:55 +0800

    splash screen

---
+++ SplashScreenDemo/SplashScreenDemo/MainActivity.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace SplashScreenDemo
+{
+    [Activity(Label = "Main Activity")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            Toast.MakeText(this, "Welcome to MainActivity", ToastLength.Long).Show();
+        }
+
+    }
+}
---
+++ SplashScreenDemo/SplashScreenDemo/SplashScreenActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views.Animations;
+using System;
+using Java.Lang;
+using Android.Graphics;
+
+namespace SplashScreenDemo
+{
+    [Activity(Label = "SplashScreenDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SplashScreenActivity : Activity
+    {
+        ImageView imageView;
+        Animation view_animation;
+        TextView textview;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(Android.Views.WindowFeatures.NoTitle);
+            SetContentView (Resource.Layout.SplashScreen);
+            imageView = (ImageView)FindViewById(Resource.Id.imageView);
+            
+            view_animation = AnimationUtils.LoadAnimation(this,Resource.Animation.hyperspace_jump);
+             
+            imageView.StartAnimation(view_animation);
+            view_animation.AnimationEnd += Rotate_AnimationEnd;
+            
+        }
+
+        private void Rotate_AnimationEnd(object sender, Animation.AnimationEndEventArgs e)
+        {
+            Finish();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+
+}
+
