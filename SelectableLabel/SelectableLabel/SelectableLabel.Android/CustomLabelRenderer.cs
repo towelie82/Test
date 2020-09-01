637e36592f97d5215caf4fa199f36e6fc8875926
wendyzang@hotmail.com <t%R8x2Qv>
2020-09-01 15:00:11 +0800

    upload SelectableLabel

---
+++ SelectableLabel/SelectableLabel/SelectableLabel.Android/CustomLabelRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SelectableLabel;
+using SelectableLabel.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SELabel), typeof(CustomLabelRenderer))]
+namespace SelectableLabel.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetTextIsSelectable(true);
+        }
+    }
+}
