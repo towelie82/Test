e6d5ecca7d9861eb6d3e6ccebbc64a994e654b53
wendyzang@hotmail.com <t%R8x2Qv>
2020-09-15 13:42:43 +0800

    upload splash screen

---
+++ SplashScreen/SplashScreen/SplashScreen.Android/Splash_Activity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Label = "Splash_Activity", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class Splash_Activity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        async void SimulateStartup()
+        {
+           
+            await Task.Delay(3000); // Simulate a bit of startup work.
+          
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
